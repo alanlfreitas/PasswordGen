056f3d228219937c88eab5249fc09886a19fa8c0
alanlfreitas <113162737+alanlfreitas@users.noreply.github.com>
2023-09-04 03:43:22 +0000

    Password gen init

--- main.py
+++ main.py
@@ -34,35 +34,55 @@
 # print(f"The highest score in the class is: {maxScore}")
 
 # -------------------- Adding numbers in RANGE() -------------------- #
-for number in range(1, 21, 3): #prints from number 1 to 20 skipping adding 3 to the previous number
-  print(number)
+# for number in range(1, 21, 3): #prints from number 1 to 20 skipping adding 3 to the previous number
+#   print(number)
 
-total = 0
-for number in range(1, 101):
-  total += number
-print(total)
+# total = 0
+# for number in range(1, 101):
+#   total += number
+# print(total)
 
-# -------------------- Adding even numbers in RANGE() -------------------- #
+# # -------------------- Adding even numbers in RANGE() -------------------- #
 
-total1 = 0
-for number in range(1, 101): #using REST to set the even numbers
-    if number % 2 == 0:
-        total1 += number
-print(total1)
+# total1 = 0
+# for number in range(1, 101): #using REST to set the even numbers
+#     if number % 2 == 0:
+#         total1 += number
+# print(total1)
 
-total2 = 0
-for number in range(2, 101, 2): #setting the parameter in the RANGE(*, *, _)
-    total2 += number
-print(total2)
+# total2 = 0
+# for number in range(2, 101, 2): #setting the parameter in the RANGE(*, *, _)
+#     total2 += number
+# print(total2)
 
-# -------------------- FizzBuzz -------------------- #
+# # -------------------- FizzBuzz -------------------- #
 
-for number in range(1, 101):
-    if(number % 3 == 0) and (number % 5 == 0):
-        print("FizzBuzz")
-    elif (number % 3 == 0):
-        print("Fizz")
-    elif (number % 5 == 0):
-        print("Buzz")
-    else:
-        print(number)
+# for number in range(1, 101):
+#     if(number % 3 == 0) and (number % 5 == 0):
+#         print("FizzBuzz")
+#     elif (number % 3 == 0):
+#         print("Fizz")
+#     elif (number % 5 == 0):
+#         print("Buzz")
+#     else:
+#         print(number)
+
+# -------------------------------------------------- Password Gen -------------------------------------------------- #
+
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
